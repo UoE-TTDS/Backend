3cfac55e0b7d26ea4796b30987415d39f01b284f
Malin <szyszka45@gmail.com>
2019-01-19 19:01:45 +0000

    fix?

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,7 +6,7 @@ api = Api()
 
 def create_app():
     app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+    #app.wsgi_app = ProxyFix(app.wsgi_app)
     from .endpoints import main_blueprint
     app.register_blueprint(main_blueprint)
     api.init_app(app)
