9a6256f95123a0d2dc1150b936b535050220d2cd
Malin <szyszka45@gmail.com>
2019-01-19 19:15:39 +0000

    logging

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+import logging
+import os
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+ try:
+    import api
 
-if __name__ =="__main__":
-    app.run()
+    if __name__ == "__main__":
+        app = api.create_app()
+        app.run(host='0.0.0.0')
+except Exception as ex:
+    logging.error('Failed start: '+ str(e))
+    
